c00c8d940315091de7e4bcc593765a876417725e
yudhalexa <101029025+yudhalexa@users.noreply.github.com>
2025-02-20 16:31:54 +0700

    Step 6 - Extracting screen composables

--- app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -36,6 +36,9 @@ import com.example.compose.rally.ui.components.RallyTabRow
 import com.example.compose.rally.ui.theme.RallyTheme
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.compose.rally.ui.accounts.AccountsScreen
+import com.example.compose.rally.ui.bills.BillsScreen
+import com.example.compose.rally.ui.overview.OverviewScreen
 
 /**
  * This Activity recreates part of the Rally Material Study from
@@ -77,18 +80,25 @@ fun RallyApp() {
                 modifier = Modifier.padding(innerPadding)
             ) {
                 composable(route = Overview.route) {
-                    Overview.screen()
+                    OverviewScreen(
+                        onClickSeeAllAccounts = {
+                            navController.navigateSingleTopTo(Accounts.route)
+                        },
+                        onClickSeeAllBills = {
+                            navController.navigateSingleTopTo(Bills.route)
+                        }
+                    )
                 }
                 composable(route = Accounts.route) {
-                    Accounts.screen()
+                    AccountsScreen()
                 }
                 composable(route = Bills.route) {
-                    Bills.screen()
+                    BillsScreen()
                 }
             }
-            Box(Modifier.padding(innerPadding)) {
-                currentScreen.screen()
-            }
+//            Box(Modifier.padding(innerPadding)) {
+//                currentScreen.screen()
+//            }
         }
     }
 }
